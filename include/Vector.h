78a2fba51b9743b88992fdcdc8a4875b42d3e199
Jade <jade@JadedeMacBook-Pro.local>
2022-03-14 22:46:25 +0800

    2022-03-14 vector.h add insert, remove

--- include/Vector.h
+++ include/Vector.h
@@ -1,8 +1,11 @@
 #ifndef DATA_STRUCTURE_LIST_H 
 
 #include <iostream>
-
-namespace mydatasturcture {
+template <typename T>
+const T& max(const T &value1, const T &value2) {
+  return (value1 > value2) ? value1 : value2;
+}
+namespace mydatastructure {
 
 #define DEFAULT_CAPACITY 10 
 typedef int Rank;
@@ -20,6 +23,16 @@ public:
   // {
   // copyFrom(arr, lo, hi);
   // }
+  Rank getSize() const {
+    return _size;
+  }
+  Rank getCapacity() {
+    return _capacity;
+  }
+  void expand();
+  Rank insert(Rank r, const T& e);
+  int remove(Rank lo, Rank hi);
+  T remove(Rank r);
 
 private:
   Rank _size; 
@@ -30,14 +43,14 @@ private:
 }
 
 template <typename T>
-mydatasturcture::Vector<T>::Vector(int c) {
+mydatastructure::Vector<T>::Vector(int c) {
   _elem = new T[_capacity = c];
   _size = 0;
   std::cout << "default constructor of Vector" << std::endl;
 }
 
 template <typename T>
-mydatasturcture::Vector<T>::Vector(const Vector<T> &vec) {
+mydatastructure::Vector<T>::Vector(const Vector<T> &vec) {
   this->_size = vec->_size;
   this->_capacity = vec->_capacity;
   for (Rank i = 0; i < _size; ++i) {
@@ -46,17 +59,17 @@ mydatasturcture::Vector<T>::Vector(const Vector<T> &vec) {
 }
 
 template <typename T>
-mydatasturcture::Vector<T>::~Vector(){
-  std::cout << "deconsturctor of Vector" << std::endl;
+mydatastructure::Vector<T>::~Vector(){
+  std::cout << "deconstructor of Vector" << std::endl;
 }
 
 template <typename T>
-T& mydatasturcture::Vector<T>::operator[](Rank r) {
+T& mydatastructure::Vector<T>::operator[](Rank r) {
   return _elem[r];
 }
 
 template <typename T>
-void mydatasturcture::Vector<T>::traverse() {
+void mydatastructure::Vector<T>::traverse() {
   // Vector<int> iv;
   std::cout << "traverse " << std::endl;
   for (int i = 0; i < _size; ++i)
@@ -64,6 +77,50 @@ void mydatasturcture::Vector<T>::traverse() {
     std::cout << _elem[i] << "\t";
   }
   std::cout << std::endl;
+}
+
+template <typename T>
+void mydatastructure::Vector<T>::expand() {
+  if (_size < _capacity) return;
+  _capacity = max(_capacity, DEFAULT_CAPACITY);
+  T *old_elem = _elem;
+  _elem = new T[_capacity << 1];  // multiple two
+  for (int i = 0; i < _size; ++i) 
+    _elem[i] = old_elem[i];
+  delete [] old_elem;
+  _capacity = _capacity << 1;
+}
+
+template <typename T>
+mydatastructure::Rank mydatastructure::Vector<T>::insert(Rank r, const T& e) {
+  expand();
+  for (Rank i = _size; i > r; --i) {
+    _elem[i] = _elem[i - 1];
+  }
+  _elem[r] = e;
+  ++_size;
+  return r;
+}
+
+// remove [lo, hi)
+template <typename T>
+int mydatastructure::Vector<T>::remove(
+  mydatastructure::Rank lo,                                    mydatastructure::Rank hi) {
+    if (lo == hi) return 0;
+    while (hi < _size)
+    {
+      _elem[lo++] = _elem[hi++];
+    }
+    _size = lo;
+    return (hi - lo);
+}
+
+// remove single value
+template <typename T>
+T mydatastructure::Vector<T>::remove(Rank r) {
+  T old_e = _elem[r];
+  remove(r, r + 1);
+  return old_e;
 }
 
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,28 @@
 #include "../include/Vector.h"
 #include <iostream>
-using mydatasturcture::Vector;
+using mydatastructure::Vector;
 
-int main() {
-  Vector<int> iv;
-  std::cout << "hello, it's vector" << std::endl;
-  for (int i = 0; i < 10; ++i) {
-    std::cout << iv[i] << "\t";
+void traverse(Vector<int>& vec) {
+  int vec_size = vec.getSize();
+  for (int i = 0; i < vec_size; ++i) {
+    std::cout << vec[i] << "\t";
   }
-  std::cout << std::endl;
+}
+int main() {
+  Vector<int> iv(10);
+  std::cout << "hello, it's vector" << std::endl;
+
+  // vector<T>::insert
+  for (int i = 0; i < 10; ++i) {
+    std::cout << iv.insert(i,i) << "\t";
+  }
   iv.traverse();
   std::cout << std::endl;
+  std::cout << "size before insertion: " << iv.getSize() << std::endl;
+  std::cout << "capacity before expanding: " << iv.getCapacity()
+            << std::endl;
+  iv.insert(1, 1);
+  std::cout << "size after insertion: " << iv.getSize() << std::endl;
+  std::cout << "capacity after expanding: " << iv.getCapacity()
+            << std::endl;
 }
